7b6585c627610e16095551f5555a890aa527be0e
Keyan <Kpesaran@gmail.com>
2024-05-06 22:30:11 -0700

    Added global as a filter option on form

--- frontend/src/components/mapPageComponents/MapDisplay.tsx
+++ frontend/src/components/mapPageComponents/MapDisplay.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useState ,useCallback } from "react"
-import axios from "axios";
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
 
-import PostInfoCard from "./SelectedPost";
+import PostInfoCard from './SelectedPost';
 
-import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps'
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
 
-import './styles.css'
-import { info } from "console";
+import './styles.css';
+import { info } from 'console';
 
-const apiKeyGoogle = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 export default function MapDisplay() {
-  const [posts, setPosts] = useState([])
-  const [infoCardVisible, setInfoCardVisible] = useState(false)
-  const [selectedPost, setSelectedPost] = useState({})
+  const [posts, setPosts] = useState([]);
+  const [infoCardVisible, setInfoCardVisible] = useState(false);
+  const [selectedPost, setSelectedPost] = useState({});
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -26,50 +25,46 @@ export default function MapDisplay() {
     };
     fetchData();
   }, []);
-  
+
   const handleMarkerClick = (post) => {
-    
     setSelectedPost(post);
     if (Object.keys(post).length === 0) {
-      setInfoCardVisible(false)
-    }
-    else setInfoCardVisible(true)
+      setInfoCardVisible(false);
+    } else setInfoCardVisible(true);
+  };
 
-  }
-
-  console.log(selectedPost)
-  console.log(infoCardVisible)
+  console.log(selectedPost);
+  console.log(infoCardVisible);
   return (
-  <>
-    
-    <div className="map-container">
-      <APIProvider apiKey=''>
-      
-        <Map
-          style={{ width: '50vw', height: '75vh' }}
-          defaultCenter={{ lat: 22.54992, lng: 0 }}
-          defaultZoom={2}
-          gestureHandling={'greedy'}
-          disableDefaultUI={true}>
-           {posts.map((post) =>
-          (
-            <Marker key = {post._id} onClick = {()=>handleMarkerClick(post)} position = {{ lat: post.lat, lng: post.lng }} />
-          )
-        )}
-            </Map >
+    <>
+      <div className='map-container'>
+        <APIProvider apiKey=''>
+          <Map
+            style={{ width: '50vw', height: '75vh' }}
+            defaultCenter={{ lat: 22.54992, lng: 0 }}
+            defaultZoom={2}
+            gestureHandling={'greedy'}
+            disableDefaultUI={true}
+          >
+            {posts.map((post) => (
+              <Marker
+                key={post._id}
+                onClick={() => handleMarkerClick(post)}
+                position={{ lat: post.lat, lng: post.lng }}
+              />
+            ))}
+          </Map>
         </APIProvider>
-    </div>
+      </div>
       <div>
-      {infoCardVisible ? <div className ="" onClick = {()=> handleMarkerClick({})}>
-    
-        <PostInfoCard post = {selectedPost} />
-        
+        {infoCardVisible ? (
+          <div className='' onClick={() => handleMarkerClick({})}>
+            <PostInfoCard post={selectedPost} />
+          </div>
+        ) : (
+          <h3>Click a Marker to display a post</h3>
+        )}
       </div>
-        
-        : <h3>Click a Marker to display a post</h3>}
-      </div>
-      
-      </>
-  )
+    </>
+  );
 }
-
