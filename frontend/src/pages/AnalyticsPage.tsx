b588e41cd703608f3ef7b68fb3803dfcfa2300ec
Keyan <Kpesaran@gmail.com>
2024-05-09 23:53:10 -0700

    began setting up async func to fetch data needed for visualization

--- frontend/src/pages/AnalyticsPage.tsx
+++ frontend/src/pages/AnalyticsPage.tsx
@@ -1,12 +1,39 @@
 
+import axios from "axios"
+import { resolve } from "path"
+import { useEffect, useState } from "react"
 
 
+const  fetchPostsData = async () => {
+    
+    try {
+        const endpoint = `http://localhost:3001/api/v1/postings?`
+
+        const response = await axios.get(endpoint)
+        console.log(response.data)
+    }
+    catch (err) {
+
+        console.error(err)
+    }
+    
+}
 
 
 export default function AnalyticsPage() {
+    const [posts, setPosts] = useState([])
 
+    useEffect(() => {
+
+    },[])
 
     return (
-        <div>Analytics Page</div>
+        <div>
+            <div>Analytics Page</div>
+            {posts.map((post) => (
+                <div key={post._id}>{post.title}</div>
+            ))}
+        </div>
+        
     )
 }
